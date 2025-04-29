7a5d8bbdff0a2ea7550c85ee080e7e5606b59352
HeuristicBitSyntaxTerminator2077 <castanares.christianlee@cec.edu.ph>
2025-04-29 10:48:58 +0800

    second commit

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,5 +8,8 @@ app = Flask(__name__)
 def home():
     return "Hello from Flask on Heroku!"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
